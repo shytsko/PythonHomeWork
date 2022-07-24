c9d3d76d246523ebf95ef16704137f75cd746081
Vladimir Shytsko <shytsko.v@gmail.com>
2022-07-24 18:59:15 +0300

    Seminar5 final

--- seminar5/task3.py
+++ seminar5/task3.py
@@ -8,31 +8,71 @@
 from random import randint
 
 
-STARTING_AMOUNT = 2021
-MAX_MOVE = 28
+startirngAmount = 521
+maxMove = 28
 
 typesGame = {1: "Игра против человека",
-             2: "Игра против бота", 3: "Игра против умного бота"}
+             2: "Игра против бота"}
 
-while True:
-    print("Выберите тип игры:")
-    for t in typesGame:
-        print(f"{t} - {typesGame[t]}")
-    try:
-        typeGame = int(input("Ваш выбор: "))
-        if typeGame not in typesGame:
-            raise
-    except:
-        print("Недопустимое значение. Повторите выбор.")
-    else:
-        break
 
-print(f"Выбрана {typesGame[typeGame]}")
+def GetGameType() -> int:
+    while True:
+        print("Выберите тип игры:")
+        for t in typesGame:
+            print(f"{t} - {typesGame[t]}")
+        try:
+            type = int(input("Ваш выбор: "))
+            if type not in typesGame:
+                raise
+        except:
+            print("Недопустимое значение. Повторите выбор.")
+        else:
+            return type
 
-amount = STARTING_AMOUNT
+
+def GetHumanMove(amount: int) -> int:
+    global maxMove
+    while True:
+        try:
+            count = int(input("Сколько конфет нужно забрать: "))
+            if count < 1 or count > min(amount, maxMove):
+                raise
+        except:
+            print("Недопустимое значение. Повторите ввод.")
+        else:
+            return count
+
+
+def GetBotMove(amount: int) -> int:
+    global maxMove
+    move = amount % (maxMove + 1) if amount % (maxMove + 1) != 0 else 1
+    print(f"Бот забрал конфет: {move}")
+    return move
+
+
+gameType = GetGameType()
+print(f"Выбрана {typesGame[gameType]}")
+
+amount = startirngAmount
 whoseMove = randint(0, 1)
-numberMove = 0
+if whoseMove == 1:
+    print("Вы ходите первым")
+else:
+    print("Ваш противник ходит первым")
 while amount > 0:
-    numberMove += 1
+    print("----------------------------")
+    print(f"Конфет осталось: {amount}")
+    whoseMove = (whoseMove + 1) % 2
     if whoseMove == 0:
-        print("Ход игрока 1")
+        print("Ваш ход.")
+        move = GetHumanMove(amount)
+    else:
+        print("Ход противника")
+        move = GetHumanMove(amount) if gameType == 1 else GetBotMove(amount)
+    amount -= move
+
+print("Конфет больше не осталось.")
+if whoseMove == 0:
+    print("Вы победили!!!")
+else:
+    print("Противник победил!!!")
