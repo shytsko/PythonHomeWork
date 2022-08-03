d5c34c35502c38650298daaba99f309b47f9eb53
Vladimir Shytsko <shytsko.v@gmail.com>
2022-08-03 14:03:55 +0300

    Update

--- seminar8/database.py
+++ seminar8/database.py
@@ -1,12 +1,3 @@
-# открыть базу (она же считает из файла)+
-# закрыть базу (запишет в файл)+
-# найти записи (выдаст наверх стоку вида id-фамилия-должность-отдел или в другом виде, но сама в консоль ничего выводить не будет)
-# удалить запись (по id сотрудника)
-# добавить запись (фамилия, телефон, должность, отдел) - должность и отдел будут задаваться текстом, но внутри будет проверка
-# на наличие таких среди уже имеющихся+
-# выдать список имеющихся отделов
-# выдать список имеющихся должностей
-
 import json
 
 
@@ -47,31 +38,57 @@ def SaveDataBase(directory):
         file.write(json.dumps(directory, ensure_ascii=False))
 
 
-def AddPerson(database, name, tel, department, position):
+def GetDepartmentID(database, department):
     if department in [n[0] for n in database["departments"].values()]:
         for k, v in database["departments"].items():
             if department == v[0]:
-                departmentID = k
-                break
+                return k
     else:
+        return None
+
+
+def AddDepartment(database, department):
+    departmentID = GetDepartmentID(database, department)
+    if departmentID is None:
         if len(database["departments"]) == 0:
             departmentID = 1
         else:
             departmentID = max(database["departments"].keys())+1
         database["departments"][departmentID] = [department]
+    return departmentID
 
+
+def GetAllDepartments(database):
+    return [(id, node[0]) for id, node in database["departments"].items()]
+
+
+def GetPositionstID(database, position):
     if position in [n[0] for n in database["positions"].values()]:
         for k, v in database["positions"].items():
             if position == v[0]:
-                positionID = k
-                break
+                return k
     else:
+        return None
+
+
+def AddPosition(database, position):
+    positionID = GetPositionstID(database, position)
+    if positionID is None:
         if len(database["positions"]) == 0:
             positionID = 1
         else:
             positionID = max(database["positions"].keys())+1
         database["positions"][positionID] = [position]
+    return positionID
 
+
+def GetAllPositions(database):
+    return [(id, node[0]) for id, node in database["positions"].items()]
+
+
+def AddPerson(database, name, tel, department, position):
+    departmentID = AddDepartment(database, department)
+    positionID = AddPosition(database, position)
     if len(database["persons"]) == 0:
         personID = 1
     else:
@@ -79,12 +96,53 @@ def AddPerson(database, name, tel, department, position):
     database["persons"][personID] = [name, tel, departmentID, positionID]
 
 
+def GetPerson(database, id):
+    if id in database["persons"]:
+        return (
+            id,
+            database["persons"][id][0],
+            database["persons"][id][1],
+            database["departments"][database["persons"][id][2]][0],
+            database["positions"][database["persons"][id][3]][0]
+        )
+    else:
+        return None
+
+
+def GetAllPersons(database):
+    return [GetPerson(database, key) for key in database["persons"].keys()]
+
+
+def FilterPerson(database, search):
+    return [k for k, v in database["persons"].items() if search.lower() in v[0].lower()]
+
+
+def RemovePerson(database, id):
+    database["persons"].pop(id, None)
+
+
 if __name__ == "__main__":
     db = OpenDataBase()
-    AddPerson(db, "Иванов", "111111111", "Снабжение", "Специалист по закупкам")
-    AddPerson(db, "Петров", "2222222", "Охрана", "Сесюрити")
-    AddPerson(db, "Сидоров", "4568477", "Снабжение", "Руководитель")
-    AddPerson(db, "Пупкин", "976865455", "Охрана", "Руководитель")
-    SaveDataBase(db)
-    print(db)
-
+    # AddPerson(db, "Иванов", "111111111", "Снабжение", "Специалист по закупкам")
+    # AddPerson(db, "Петров", "2222222", "Охрана", "Сесюрити")
+    # AddPerson(db, "Сидоров", "4568477", "Снабжение", "Руководитель")
+    # AddPerson(db, "Иванков", "976865455", "Ай Ти", "Программист")
+    print(GetAllDepartments(db))
+    print()
+    print(GetAllPositions(db))
+    print()
+    print(GetAllPersons(db))
+    print()
+    # filter = FilterPerson(db, "Иван")
+    # for id in filter:
+    #     print(GetPerson(db, id))
+    # RemovePerson(db, 5)
+    # print(GetAllPersons(db))
+    # print()
+    # RemovePerson(db, 55)
+    # print()
+    # filter = FilterPerson(db, "Иван")
+    # for id in filter:
+    #     print(GetPerson(db, id))
+    # SaveDataBase(db)
+    # print(db)
